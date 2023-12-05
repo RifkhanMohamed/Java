4ba96ecd27099a74f0204cb0543a7777cb39dc84
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2023-12-05 12:42:55 +0000

    Internationalization

---
+++ Internationalization.java
@@ -0,0 +1,32 @@
+
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Internationalization {
+
+    public static void display(String language, String country) {
+        try {
+            Locale locale = new Locale(language, country);
+            int num = 123456;
+            NumberFormat nm = NumberFormat.getInstance(locale);
+            System.out.println("Number Format: " + nm.format(num));
+            DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, locale);
+            System.out.println("Date Format: " + df.format(new Date()));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        display("en", "US");
+        System.out.println("==========================");
+        display("en", "UK");
+        System.out.println("==========================");
+        display("ja", "JP");
+        System.out.println("==========================");
+        display("fr", "FR");
+    }
+}
